040efcf89532c22cf12c09cdba7375fe5cc5ba86
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-05-22 10:50:04 +0200

    Exo3 done

--- script.js
+++ script.js
@@ -67,19 +67,34 @@ const box3 = document.createElement("div")
 
 box3.classList.add("box3")
 
-document.onkeydown = function(event){
+let i = 0 // nombres de box qu'on incrémente à chaque ArrowUp.
 
-    if (event.key == "ArrowUp"){ // .keyCode est deprecated. On utilise key et le texte ArrowUp. Attention à la casse.
+document.onkeydown = function(event){    
 
-        let newBox3 = box3.cloneNode()
-        parentEl3.appendChild(newBox3)
+    if (event.key == "ArrowDown"){ // .keyCode est deprecated. On utilise key et le texte ArrowDown. Attention à la casse.
 
-        newBox3.style.backgroundColor = "#" + (Math.floor(Math.random()*16777215).toString(16))
+        i++ // On incrémente i défini plus haut. Ce qui donnera des box avec des id style "box1", "box2" etc
 
-        
+        let newBox3 = box3.cloneNode() // On clone notre box déclarée au début.
+
+        newBox3.id = "currBox" + i // Et on lui attribue un id spécifique dont le nom sera currBox1, puis currBox2 etc vu qu'on le concatène avec i.
+
+        parentEl3.appendChild(newBox3) // Et on la fait apparaître.
+
+        // on fait un floor d'un chiffre random (entre 0 et 1 * 16m de couleurs) qu'on ressort en tostring (base 16) ce qui sortira une suite de caractères hexadecimaux. Soit des chiffres, soit des lettres.
+        newBox3.style.backgroundColor = "#" + (Math.floor(Math.random()*16777215).toString(16)) // Attribution d'une couleur aléatoire au style CSS
+       
     } 
+    
+    let currBox = document.getElementById(("currBox" + i)) // Pour la suite, on récupère le dernier élément créé en concaténant "currBox" avec le i actuel dans le script.
 
-}  // IN PROGRESS. Les box se créent mais ne se delete pas encore.
+    if(event.key =="ArrowUp"){ // Si la douche arrowUp est pressée, on remove la box en cours qu'on a selectionné par son id au dessus
+
+        currBox.remove() // suppression de la box
+
+        i-- // On décrémente i pour pouvoir accéder à la box qui précède celle qu'on vient de supprimer.
+    }
+}  
 
 
 
